b0157c0416fef16cc67feb05bc0c638cc1635f98
TomaPartridge <80053439+TomaPartridge@users.noreply.github.com>
2021-06-09 05:09:32 +1000

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,15 +1,28 @@
-package com.coveros.demo.helloworld;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import java.util.Scanner;
 
-public class HelloWorld {
+public class TheFirstClass {
 
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
+    public static void main(String[] args) {
+        System.out.println("Уиии");
 
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
-  }
+        Scanner in=new Scanner(System.in);
+        int num=in.nextInt();
+        System.out.printf("3 numbers %d \n",num*3);
+        in.close();
 
+        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+        final LocalDateTime now = LocalDateTime.now();
+
+        System.out.println("Hello, World! The current time is " + dtf.format(now));
+
+        String john = null;
+        Optional<String> name2 = Optional.ofNullable(john);
+        if (name2.isPresent()) {
+            System.out.println(name2 + " ok");
+        }
+
+    }
 }
